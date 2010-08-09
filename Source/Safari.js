a4d79f4f268e9a8256c9829b7a307535e3ef68c2
Thomas Aylott <oblivious+git@subtlegradient.com>
2010-08-09 16:23:28 -0400

    Safari.addCSS, Safari.removeCSS

--- Source/Safari.js
+++ Source/Safari.js
@@ -80,6 +80,82 @@ Safari.command.reloadCSS = function(){
 	}).join('\n');
 };
 
+Safari.command.removeCSS = function(){
+	return Array.prototype.map.call(document.querySelectorAll('link[rel=stylesheet][href],style'),function(css){
+		css.disabled = true;
+		css.parentNode.removeChild(css);
+		return css;
+	}).join('\n');
+};
+Safari.command.addCSS = function(){
+	
+	/*
+	---
+	name : Sheet.DOM
+	description : Sheet.DOM adds some handy stuff for working with the browser's native CSS capabilities.
+
+	authors   : Thomas Aylott
+	copyright : © 2010 Thomas Aylott
+	license   : MIT
+
+	provides : Sheet.DOM
+	...
+	*/
+	
+	if (typeof Sheet == 'undefined') var Sheet = {};
+	if (Sheet.DOM == null) Sheet.DOM = {};
+
+	window.Sheet = Sheet;
+	
+	Sheet.DOM.createSheet = function(raw){
+		var oldLength = document.styleSheets.length;
+
+		if (document.createStyleSheet){
+			document.createStyleSheet();
+			document.styleSheets[document.styleSheets.length - 1].cssText = raw;
+		}
+
+		if (oldLength >= document.styleSheets.length){
+			var style = document.createElement('style');
+			style.setAttribute('type','text/css');
+			style.appendChild(document.createTextNode(raw));
+			document.getElementsByTagName('head')[0].appendChild(style);
+		}
+
+		if (oldLength >= document.styleSheets.length){
+			var style = document.createElement('div');
+			style.innerHTML = '<style type="text/css">' + raw + '</style>';
+			document.getElementsByTagName('head')[0].appendChild(style);
+		}
+
+		if (oldLength >= document.styleSheets.length)
+			throw new Error('no styleSheet added :(');
+
+		var sheet = document.styleSheets[document.styleSheets.length - 1];
+		sheet.cssText = raw;
+
+		return sheet;
+	};
+
+	// Sheet.DOM.createSheet = function(raw){
+	// 	var style = document.createElement('style');
+	// 	style.setAttribute('type','text/css');
+	// 	style.appendChild(document.createTextNode(raw));
+	// 	document.getElementsByTagName('head')[0].appendChild(style);
+	// 	
+	// 	return document.styleSheets[document.styleSheets.length - 1];
+	// };
+
+	Sheet.DOM.createStyle = function(raw){
+		var div = document.createElement('div');
+		div.innerHTML = '<div style="' + raw + '"></div>';
+		return { style:div.firstChild.style };
+	};
+};
+Safari.prototype.setCSS = function(css){
+	this.exec([uneval(Safari.command.addCSS), uneval(Safari.command.removeCSS), uneval('Sheet.DOM.createSheet("' + escape.String.escapeJS(css) + '")')].join(';'));
+};
+
 
 return exports;
 }(typeof exports != 'undefined' ? exports : this))
